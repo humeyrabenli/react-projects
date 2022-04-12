a159d010c5b8acb9aa8cc758ec8ddade9bc076ab
humeyrabenli <humeyrabenli98@gmail.com>
2022-04-12 13:29:04 +0300

    update

--- react/mui-menu/reacttest/src/App.tsx
+++ react/mui-menu/reacttest/src/App.tsx
@@ -1,4 +1,4 @@
-import React,{useState} from 'react';
+import React,{useRef, useState} from 'react';
 import './App.css';
 import NestedMenuItem from './components/NestedMenuItem';
 import MenuItem from "@mui/material/MenuItem";
@@ -7,62 +7,54 @@ import Typography from "@mui/material/Typography";
 
 
 function App() {
-  const [menuPosition, setMenuPosition] = useState<any>(null);
+  const [menuPosition, setMenuPosition] = useState<any>({top: 10, left: 10})
+  const menuItemRef = useRef<any>(null)
 
   const handleRightClick = (event: React.MouseEvent) => {
     if (menuPosition) {
-      return;
+      return
     }
-    event.preventDefault();
+    event.preventDefault()
     setMenuPosition({
       top: event.pageY,
       left: event.pageX
-    });
-  };
+    })
+  }
 
   const handleItemClick = (event: React.MouseEvent) => {
-    setMenuPosition(null);
-    console.log("Hello");
-  };
-
+    setMenuPosition(null)
+  }
   return (
-    <div onContextMenu={handleRightClick} onClick={handleRightClick}>
-      <Typography>Right/left click to open menu</Typography>
-      <Menu
-        open={!!menuPosition}
-        onClose={() => setMenuPosition(null)}
-        anchorReference="anchorPosition"
-        anchorPosition={menuPosition}
+    <div onContextMenu={handleRightClick}>
+    <Typography>Right click to open menu</Typography>
+    <Menu
+      open={!!menuPosition}
+      onClose={() => setMenuPosition(null)}
+      anchorReference='anchorPosition'
+      anchorPosition={menuPosition}
+    >
+      <MenuItem onClick={handleItemClick}>Button 1</MenuItem>
+      <MenuItem onClick={handleItemClick}>Button 2</MenuItem>
+      <NestedMenuItem
+        ref={menuItemRef}
+        label='Button 3'
+        parentMenuOpen={!!menuPosition}
+        onClick={handleItemClick}
       >
-        <MenuItem onClick={handleItemClick}>Button 1</MenuItem>
-        <MenuItem onClick={handleItemClick}>Button 2</MenuItem>
+        <MenuItem onClick={handleItemClick}>Sub-Button 1</MenuItem>
+        <MenuItem onClick={handleItemClick}>Sub-Button 2</MenuItem>
         <NestedMenuItem
-          label="Button 3"
+          label='Sub-Button 3'
           parentMenuOpen={!!menuPosition}
-          // onClick={handleItemClick}
+          onClick={handleItemClick}
         >
-          <MenuItem onClick={handleItemClick}>Sub-Button 1</MenuItem>
-          <MenuItem onClick={handleItemClick}>Sub-Button 2</MenuItem>
-          <NestedMenuItem
-            label="Sub-Button 3"
-            parentMenuOpen={!!menuPosition}
-            // onClick={handleItemClick}
-          >
-            <MenuItem onClick={handleItemClick}>Sub-Sub-Button 1</MenuItem>
-            <MenuItem onClick={handleItemClick}>Sub-Sub-Button 2</MenuItem>
-          </NestedMenuItem>
+          <MenuItem onClick={handleItemClick}>Sub-Sub-Button 1</MenuItem>
+          <MenuItem onClick={handleItemClick}>Sub-Sub-Button 2</MenuItem>
         </NestedMenuItem>
-        <MenuItem onClick={handleItemClick}>Button 4</MenuItem>
-        <NestedMenuItem
-          label="Button 5"
-          parentMenuOpen={!!menuPosition}
-          // onClick={handleItemClick}
-        >
-          <MenuItem onClick={handleItemClick}>Sub-Button 1</MenuItem>
-          <MenuItem onClick={handleItemClick}>Sub-Button 2</MenuItem>
-        </NestedMenuItem>
-      </Menu>
-    </div>
+      </NestedMenuItem>
+      <MenuItem onClick={handleItemClick}>Button 4</MenuItem>
+    </Menu>
+  </div>
   );
 }
 
